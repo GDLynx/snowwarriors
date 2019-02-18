53e9b969fb04ade565f2224a56a8a31e8caa854c
gdlynx <lynxthatthinks@gmail.com>
2019-02-18 03:38:40 +0000

    add click to damage enemy functionallity

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const http = require("http");
 const port =  3000; 
 
 let num = 0; 
+let enemyHealth = 100; 
 
 const server = http.createServer((req, res) => { 
-    console.log(`Listening on port ${port}`)
+    // console.log(`Listening on port ${port}`)
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "text/json");    
     switch (req.url) { 
@@ -13,20 +14,25 @@ const server = http.createServer((req, res) => {
             if (req.method == "GET" || req.method == "get") { 
                 res.write(JSON.stringify(
                     { 
-                        content: num
+                        content: num, 
+                        enemy: 
+                            { 
+                                health: enemyHealth
+                            } 
                     }
                 )); 
                 res.end();
             } else if (req.method == "POST" || req.method == "post") { 
                 let body = '';
-                req.on('data', function (data) {
+                req.on('data', data => {
                     body += data;
                     if (body.length > 1e6) req.connection.destroy();
                 });
 
-                req.on('end', function () {
-                    console.log(JSON.parse(body).content); 
-                });
+                req.on('end', () => {
+                    // console.log(JSON.parse(body).content); 
+                    enemyHealth -= JSON.parse(body).damage; 
+                }); 
                 num++; 
             }
             break; 
