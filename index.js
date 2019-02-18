7b4ca2300d357e7cb8de67d49b30bf101f86345d
gdlynx <lynxthatthinks@gmail.com>
2019-02-18 03:07:35 +0000

    able to read incoming json flexibly

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http"); 
-const { parse } = require('querystring');
+// const { parse } = require('querystring');
+const qs = require("querystring"); 
 let num = 0; 
 const port =  3000; 
 const server = http.createServer((req, res) => { 
@@ -17,6 +18,27 @@ const server = http.createServer((req, res) => {
                 )); 
                 res.end();
             } else if (req.method == "POST" || req.method == "post") { 
+                let body = '';
+
+                req.on('data', function (data) {
+                    // console.log(data); 
+                    console.log('' + data) 
+                    body += data;
+
+                    // Too much POST data, kill the connection!
+                    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                    if (body.length > 1e6)
+                        req.connection.destroy();
+                });
+
+                req.on('end', function () {
+                    console.log(JSON.parse(body).content); 
+                    let post = qs.parse(body);
+                    // console.log(post); 
+                    // console.log(post.content); 
+                    // console.log(post["content"]); 
+                });
+                /* 
                 let body = [];
                 req.on('data', (chunk) => {
                     body.push(chunk);
@@ -32,6 +54,7 @@ const server = http.createServer((req, res) => {
                         console.log("yay snow wizard"); 
                     }
                 });
+                */ 
                 num++; 
             }
             break; 
@@ -44,7 +67,8 @@ function collectRequestData(request, callback) {
         body += chunk.toString();
     });
     request.on('end', () => {
-        callback(parse(body));
+        // callback(parse(body));
+        callback(qs.parse(body)); 
     });
 }
 
